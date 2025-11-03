99836ec4a416538a031698e6607d59ebf3f85b40
Meli248 <melishashakya248@gmail.com>
2025-11-03 22:42:46 +0545

    Add control flow, list, and operators practice files

--- app/src/main/java/com/example/meli/practice/list.kt
+++ app/src/main/java/com/example/meli/practice/list.kt
@@ -1,2 +1,24 @@
 package com.example.meli.practice
+fun main(){
+        //mutable ma reassign garna milcha ani int string sab capital
+        var name: String = "meli"
+        name = "salifa"
+    println(name)
+      //immutable
+        val age: Int = 16
+        //to print use print or println
+        println("my name is $name and age is $age") //salifa ko naam huncha as its reassigned
+        //variable ko aru ni access garne vaye {$name.uppercase()}
+    //immutable
+    val number =listOf("five","nine","fifty","eight")
+    println ("list")
+    for (i in number.indices){// so like indices gives all the valid positions (indexes)
+        println(number[i])
+    }
+    println()
+
+
+}
+
+
 
--- app/src/main/java/com/example/meli/practice/operators.kt
+++ app/src/main/java/com/example/meli/practice/operators.kt
@@ -1,2 +1,43 @@
 package com.example.meli.practice
+fun main() {
+    var a:Int=2
+    var b:Int=3
+    var c:Int =0
+    //arithmetic operator
+    c=a+b
+    println("a+b=$c")
+    c+= a
+    println("c+=$c")
+    c-=a
+    println("c-=a=$c")
+    c*=a
+    println("c*=a=$c")
+    c/=a
+    println("c/=a=$c")
+
+    //unary operator
+    var num: Double = 3.4
+    var isCheck: Boolean=true;
+    println("+numbber=${+num}")
+    println("-numbber=${-num}")
+    println("++numbber=${++num}")//increment
+    println("--numbber=${-+num}")//decrement
+    println("!isCheck=${!isCheck}")//complement
+
+
+
+        // Console input
+        print("Enter student: ")
+        var student: String = readln();
+        print("Enter marks: ")
+        val marks: Int = readln()!!.toInt() //!! means the unput will not be null  and toInt will convert string to int
+        println("student name: $student, Age: $marks")
+}
+
+
+
+
+
+
+
 
